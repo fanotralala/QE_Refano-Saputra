e121078b32d6ef17c813323d1c72896a5a9ca399
fanotralala <velgo.refa@gmail.com>
2023-03-11 16:12:15 +0700

    sec 5

--- 05_Basic_Linux_Command/praktikum/script.sh
+++ 05_Basic_Linux_Command/praktikum/script.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-
-echo 'umur berapa skrg broo'
-
-read umur
-if [ $umur -ge 21 ]
-then
-    echo 'udh boleh party'
-else
-    echo 'blom boleh sama papa'
-fi
+if(umur < 21){
+            System.out.println("udh boleh party");
+          }else{
+            System.out.println("blom boleh sama papa");
+          }
